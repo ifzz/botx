1bcb8624cf6a225f24965a54d78499bd739ed9d4
Andrew <contact@andrewpd.com>
2018-02-18 22:30:16 +0800

    add request frequence

--- src/main.go
+++ src/main.go
@@ -334,7 +334,7 @@ func Start(bot *Bot, exchangeCfg SExchange) {
 	//orderMap := make(map[string]string) //记录所有成交对
 	for systemExit == false {
 
-		time.Sleep(1.1537 * time.Second)
+		time.Sleep(7539 * time.Millisecond)
 
 		acct, err := bot.Exchange.GetAccount()
 		if err != nil {
